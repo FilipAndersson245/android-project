a4479fc56648445c16bf818e8e35caf7c60995c4
FilipAndersson245 <jabos123@gmail.com>
2019-02-17 21:41:25 +0100

    Worked on developing a drawer.

--- app/src/main/java/se/ju/myapplication/MainActivity.java
+++ app/src/main/java/se/ju/myapplication/MainActivity.java
@@ -1,7 +1,20 @@
 package se.ju.myapplication;
 
+import android.annotation.SuppressLint;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,20 +25,53 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meme_list);
+        setContentView(R.layout.drawer);
 
-        ArrayList<Meme> al=new ArrayList<>();
-        al.add(new Meme(1,1,"bob", "PLG #mirror","https://schpoopstorage.s3.eu-central-1.amazonaws.com/memes/1895d1ff-f021-44a0-b814-02f4e951c4f0",5, new Timestamp(1549106319)));
-        al.add(new Meme(2,1,"bob2", "swag","https://schpoopstorage.s3.eu-central-1.amazonaws.com/memes/0a07f11c-602a-4b00-a6c3-b54da107a413",32, new Timestamp(1549106336)));
-        al.add(new Meme(2,1,"bob2", "Ow is dead","https://schpoopstorage.s3.eu-central-1.amazonaws.com/memes/807eccc2-16ce-4c77-9c25-6176421d8a0c",64, new Timestamp(1549106324)));
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        assert actionbar != null;
+        actionbar.setDisplayShowTitleEnabled(false);
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
 
+        mDrawer = findViewById(R.id.drawer_layout);
+        nvDrawer = findViewById(R.id.nvView);
+    }
 
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(new MemeViewAdapter(al,this));
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void burger_clicked(View view) {
     }
 }
