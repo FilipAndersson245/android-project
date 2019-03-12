ba9421735fa6c6d0a58710ea5db4f0aedceccdc3
zimonitrome <zimonitrome@gmail.com>
2019-03-12 17:26:08 +0100

    Fixed camera for phones from this millennia.

--- app/src/main/java/se/ju/myapplication/Create/MemeTemplate/CreateMemeTemplateActivity.java
+++ app/src/main/java/se/ju/myapplication/Create/MemeTemplate/CreateMemeTemplateActivity.java
@@ -1,7 +1,7 @@
 package se.ju.myapplication.Create.MemeTemplate;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -14,6 +14,7 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -23,11 +24,14 @@ import java.io.File;
 
 import se.ju.myapplication.R;
 
+import static android.support.v4.content.FileProvider.getUriForFile;
 import static android.widget.Toast.makeText;
 
 public class CreateMemeTemplateActivity extends Activity {
+
     private static final int TAKE_PICTURE = 1;
-    private static final int PERMISSION_REQUEST_CODE = 2;
+    private static final int PERMISSION_FILES_CODE = 1;
+    private static final int PERMISSION_CAMERA_CODE = 2;
 
 
     private Uri imageUri;
@@ -38,55 +42,77 @@ public class CreateMemeTemplateActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meme_template);
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (!checkPermission()) {
-                requestPermission();
-            }
-        } else {
-            // requestPermission();
-            makeText(this, R.string.no_file_access, Toast.LENGTH_LONG).show();
-            onBackPressed();
-        }
 
 
         this.templateImage = findViewById(R.id.createMemeTemplateImage);
     }
 
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    private boolean checkPermission(String permission) {
+        int result = ContextCompat.checkSelfPermission(this, permission);
         return result == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void requestPermission() {
+    private void requestPermission(String permission, int responseCode) {
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+            // Permission is not granted
             makeText(this, getString(R.string.external_storage_explaination), Toast.LENGTH_LONG).show();
         } else {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(this, new String[]{permission}, responseCode);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
+            case PERMISSION_FILES_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.e("value", "Permission Granted, Now you can use local drive .");
                 } else {
                     Log.e("value", "Permission Denied, You cannot use local drive .");
-                    onBackPressed();
+                }
+                break;
+            case PERMISSION_CAMERA_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("value", "Permission Granted, Now you can use local drive .");
+                } else {
+                    Log.e("value", "Permission Denied, You cannot use local drive .");
                 }
                 break;
         }
     }
 
     public void takePhoto(View view) {
+        if (Build.VERSION.SDK_INT < 23) {
+            makeText(this, R.string.no_file_access, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!checkPermission(Manifest.permission.CAMERA)) {
+            requestPermission(android.Manifest.permission.CAMERA, PERMISSION_CAMERA_CODE);
+            return;
+        }
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                Uri.fromFile(photo));
-        imageUri = Uri.fromFile(photo);
+        File photo = new File(new File(this.getFilesDir(), "images"), "Pic.jpg");
+        Uri imageUri = getUriForFile(this, "se.ju.myapplication.fileprovider", photo);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    public void selectPhoto(View view) {
+        if (Build.VERSION.SDK_INT < 23) {
+            makeText(this, R.string.no_file_access, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (!checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            requestPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, PERMISSION_FILES_CODE);
+            return;
+        }
+
+        makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
     }
 
     @Override
